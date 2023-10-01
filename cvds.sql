28f8c2f5b513ef6d72edace9c22433910c749c9e
JOHANN AMAYA LOPEZ <40411821+JohannBulls@users.noreply.github.com>
2023-10-01 16:44:28 -0500

    USANDO SPRING DATA DESDE CERO

---
+++ cvds.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-10-2023 a las 23:36:23
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cvds`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `employee`
+--
+
+CREATE TABLE `employee` (
+  `EMPLOYEE_ID` varchar(255) NOT NULL,
+  `FIRST_NAME` varchar(255) NOT NULL,
+  `LAST_NAME` varchar(255) NOT NULL,
+  `ROLE` varchar(255) NOT NULL,
+  `SALARY` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`EMPLOYEE_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/main/java/co/edu/escuelaing/cvds/lab7/model/Employee.java
@@ -0,0 +1,94 @@
+package co.edu.escuelaing.cvds.lab7.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee {
+    @Id
+    @Column(name = "EMPLOYEE_ID")
+    private String employeeId;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "ROLE")
+    private String role;
+
+    @Column(name = "SALARY")
+    private Double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String employeeId, String firstName, String lastName, String role, Double salary) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+        this.salary = salary;
+    }
+
+    // Getters y setters
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((employeeId == null) ? 0 : employeeId.hashCode());
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + ((role == null) ? 0 : role.hashCode());
+        result = prime * result + ((salary == null) ? 0 : salary.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (employeeId == null) {
+            if (other.employeeId != null)
+                return false;
+        } else if (!employeeId.equals(other.employeeId))
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        if (role == null) {
+            if (other.role != null)
+                return false;
+        } else if (!role.equals(other.role))
+            return false;
+        if (salary == null) {
+            if (other.salary != null)
+                return false;
+        } else if (!salary.equals(other.salary))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [employeeId = " + employeeId + ", firstName = " + firstName + ", lastName = " + lastName
+                + ", role = " + role + ", salary = " + salary + "]";
+    }
+}
