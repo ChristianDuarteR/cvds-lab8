daf31e0b824ae20e1d1a0a663b065b51ee4fa7ea
JOHANN AMAYA LOPEZ <40411821+JohannBulls@users.noreply.github.com>
2023-10-26 17:45:22 -0500

    Update Employee.java

--- src/main/java/co/edu/escuelaing/cvds/lab7/model/Employee.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/model/Employee.java
@@ -1,11 +1,8 @@
 package co.edu.escuelaing.cvds.lab7.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
+import jakarta.persistence.*;
 import lombok.Builder;
+
 import java.util.Objects;
 
 @Builder
@@ -31,16 +28,55 @@ public class Employee {
     public Employee() {
     }
 
-    public Employee(Long employeeId, String firstName, String lastName, String role, Double salary) {
+    public Employee (Long employeeId) {
         this.employeeId = employeeId;
-        this.firstName = firstName;
+    }
+
+     public Employee(Long employeeId, String firtsName, String lastName, String role, double salary) {
+        this.employeeId = employeeId;
+        this.firtsName = firtsName;
+        this.lastName = lastName;
+        this.role = role;
+        this.salary = salary;
+    }
+    public Employee( String firtsName, String lastName, String role, double salary) {
+        this.firtsName = firtsName;
         this.lastName = lastName;
         this.role = role;
         this.salary = salary;
     }
 
-    public void setFirstName(String firstName){
-        this.firstName = firstName;
+   
+    // Getters
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirtsName() {
+        return firtsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    // setters
+
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setFirtsName(String firtsName) {
+        this.firtsName = firtsName;
     }
 
     public void setLastName(String lastName) {
@@ -51,86 +87,21 @@ public class Employee {
         this.role = role;
     }
 
-    public void setSalary(String salary) {
-        this.salary = Double.valueOf(salary);
+    public void setSalary(double salary) {
+        this.salary = salary;
     }
 
-    public void setEmployeeId(Long id){
-        this.employeeId = id;
-    }
 
-    public Long getEmployeeId(){
-        return employeeId;
-    }
-
-    public String getFirstName(){
-        return firstName;
-    }
-
-    public String getLastName(){
-        return lastName;
-    }
-
-    public String getRole(){
-        return role;
-    }
-
-    public Double getSalary(){
-        return salary;
+    // equals and hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee employee)) return false;
+        return Double.compare(salary, employee.salary) == 0 && Objects.equals(employeeId, employee.employeeId) && Objects.equals(firtsName, employee.firtsName) && Objects.equals(lastName, employee.lastName) && Objects.equals(role, employee.role);
     }
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((employeeId == null) ? 0 : employeeId.hashCode());
-        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-        result = prime * result + ((role == null) ? 0 : role.hashCode());
-        result = prime * result + ((salary == null) ? 0 : salary.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Employee other = (Employee) obj;
-        if (employeeId == null) {
-            if (other.employeeId != null)
-                return false;
-        } else if (!employeeId.equals(other.employeeId))
-            return false;
-        if (firstName == null) {
-            if (other.firstName != null)
-                return false;
-        } else if (!firstName.equals(other.firstName))
-            return false;
-        if (lastName == null) {
-            if (other.lastName != null)
-                return false;
-        } else if (!lastName.equals(other.lastName))
-            return false;
-        if (role == null) {
-            if (other.role != null)
-                return false;
-        } else if (!role.equals(other.role))
-            return false;
-        if (salary == null) {
-            if (other.salary != null)
-                return false;
-        } else if (!salary.equals(other.salary))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee [employeeId = " + employeeId + ", firstName = " + firstName + ", lastName = " + lastName
-                + ", role = " + role + ", salary = " + salary + "]";
+        return Objects.hash(employeeId, firtsName, lastName, role, salary);
     }
 }
